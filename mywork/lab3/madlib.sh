5c3eb059fc4325ecb2297a397a5269fa2758b4fa
Cole Anderson <coleanderson@Coles-MacBook-Air.local>
2024-02-21 09:23:59 -0500

    pushing my madlib into /Users/coleanderson/ds2002-course/mywork/lab3

---
+++ mywork/lab3/madlib.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+clear
+echo "Let's build a mad-lib!"
+
+read -p "1. Please give me an adjective: " ADJ1
+read -p "2. Please give me an adverb: " ADV1
+read -p "3. Please give me an noun: " NOUN1
+read -p "4. Please give me an verb: " VERB1
+read -p "5. Please give me another adjective: " ADJ2
+read -p "6. Please give me another adverb: " ADV2
+read -p "7. Please give me another noun: " NOUN2
+read -p "8. Please give me another verb: " VERB2
+read -p "9. Please give me a third adjective: " ADJ3
+read -p "10. Please give me a third adverb: " ADV3
+read -p "11. Please give me a third noun: " NOUN3
+read -p "12. Please give me a third verb: " VERB3
+
+echo "Once upon a time there was a $ADJ1 $NOUN1."
+
+echo "One day, the $ADJ1 $NOUN1 walks in the forest, and he comes across a $ADJ2 $NOUN2."
+
+echo "The $ADJ1 $NOUN1 freaks out and $ADV1 $VERB1."
+
+echo "The $ADJ1 $NOUN1 has never seen anything like this before. All of sudden, the $ADJ2 $NOUN2 $ADV2 $VERB2 while the $ADJ1 $NOUN1 $ADV1 $VERB1."
+
+echo "Then, out of nowhere, a $ADJ3 $NOUN3 appears and $ADV3 $VERB3."
+
+echo "Now, the $ADJ1 $NOUN1, the $ADJ2 $NOUN2, and the $ADJ3 $NOUN3 are in a predicament. They are lost in the forest, don't know each other, and don't know where they are."
+
+echo "So, they decide to part ways, and they never see each other again."
+
+ 
